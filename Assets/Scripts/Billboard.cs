92250c57d08a4d87951732562adf7bdf6a1dfb73
Max Wiens <maxawiens@gmail.com>
2020-10-17 16:23:06 -0500

    fixed sprite scaling

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -3,7 +3,6 @@
 public class Billboard : MonoBehaviour {
 	void LateUpdate(){
 		Vector3 v = Camera.main.transform.forward;
-		Debug.Log(v);
 		if(v.y < -0.2f) v.y = -0.2f;
 		transform.forward = v;
 	} 
--- Assets/Scripts/Player/Ghost.cs
+++ Assets/Scripts/Player/Ghost.cs
@@ -4,22 +4,38 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Ghost : MonoBehaviour {
-  [SerializeField]
-  private Collider _triggerCollider;
-  //private 
+	public float HauntRadius = 1;
 
-  private void Start() {
-    
-  }
-  private void Update() {
-     
-     //Collider[] colliders = Physics.OverlapSphere(transform.position, FindInteractableDistance, (1 << LayerMask.NameToLayer("Interactible")) | (1 << LayerMask.NameToLayer("Haunt")));
-     //foreach()
-  }
-  
-  private IEnumerator ShowInteractable(){
-    yield break;
-  }
+	private Haunt _hauntTarget;
 
-  
+	private void Start() {
+		
+	}
+
+	private void Update() {   
+		 Collider[] colliders = Physics.OverlapSphere(transform.position, HauntRadius, (1 << LayerMask.NameToLayer("Interactible")) | (1 << LayerMask.NameToLayer("Haunt")));
+		 
+
+		foreach(Collider c in colliders){
+			Haunt h = c.GetComponent<Haunt>();
+			if(h != null){
+				_hauntTarget = h;
+				ShowInteractable();
+				break;
+			}
+		}
+		 
+	}
+	
+	private IEnumerator ShowInteractable(){
+		// h.transform
+		// yield break;
+
+		yield return new WaitUntil(() =>
+		{
+				return Vector3.Distance(transform.position, _hauntTarget.transform.position) < HauntRadius;
+		});
+	}
+
+	
 }

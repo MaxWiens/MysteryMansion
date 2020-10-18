8ab28da8261d8868aba67b13529b40be78bf8173
Pyrrhic <nefalt2@gmail.com>
2020-10-18 08:55:11 -0500

    Add quality buttons

---
+++ Assets/Scripts/UI/QualityButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QualityButton : MonoBehaviour
+{
+    public int level;
+
+    public void OnClick()
+    {
+        if (QualitySettings.GetQualityLevel() != level)
+        {
+            QualitySettings.SetQualityLevel(level, true);
+        }
+    }
+}
